21bbc6fd2095bdd58e743fe1a6e0e458101d74a0
lxf <1130034920@qq.com>
2017-04-11 14:26:33 +0800

    vue2+swiper
    图片轮播

---
+++ vue2-swiper/src/components/Hello.vue
@@ -0,0 +1,66 @@
+<template>
+   <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+               <img src="http://img.wanyx.com/upload/picture/image/20131211/20131211110356_90032.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://www.005.tv/uploads/allimg/170324/204932AD-0.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://android.tgbus.com/download/uploadfiles_2115/201205/20120521102143342.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://img2.niutuku.com/desk/1208/1510/ntk-1510-24060.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://file.mumayi.com/forum/201505/06/130921u1aikza8h2iaifie.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://pic1.win4000.com/wallpaper/f/58b124b825676.jpg" alt="">
+            </div>
+            
+            <div class="swiper-slide">
+                <img src="http://img2.niutuku.com/desk/1207/1005/ntk122712.jpg" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
+</template>
+
+<script>
+import '../assets/lib/js/swiper.js'
+export default {
+  data () {
+    return {
+     
+    }
+  },
+  mounted() {
+       var mySwiper = new Swiper ('.swiper-container', {
+         loop: true,
+         autoplay : 2000,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+      })        
+    }
+}
+</script>
+
+<style>
+    .swiper-container {
+        width: 100%;
+    }
+    .swiper-slide {
+       
+    }
+    .swiper-slide img{
+        width:100%;
+    }
+    .swiper-pagination-bullet{
+        background:green !important;
+    }
+    .swiper-pagination-bullet-active{
+       background:red !important;
+    }
+</style>
---
+++ vue2-swiper/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/lib/css/reset.css'
+import './assets/lib/css/swiper.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
